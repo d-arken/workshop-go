85eb94b16bdb1a3e7744cbfa734eccdf0ce9bbe2
Matheus Bortoletto <matheus@suaquadra.com.br>
2024-04-06 16:47:48 -0300

    4_panicrecovery / middlewares

---
+++ 4_panicrecovery/example/go.mod
@@ -0,0 +1,3 @@
+module github.com/d-arken/workshop-go/tree/main/4_panicrecovery/example
+
+go 1.22.2
---
+++ 4_panicrecovery/example/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type A struct {
+	b string
+}
+
+func LatencyMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		c.Next()
+		latency := time.Since(t)
+		log.Print(latency)
+	}
+}
+
+func main() {
+	gin.ForceConsoleColor()
+
+	r := gin.New()
+
+	r.Use(gin.Recovery())
+	r.Use(LatencyMiddleware())
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	r.GET("/panic", func(c *gin.Context) {
+		var a *A
+		print(a.b)
+
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
+
+}
